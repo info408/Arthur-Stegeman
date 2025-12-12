03166ee8ace30bcaf01c42bd262f32c259e5f24e
King Kong Movers - Gebr.Stegeman <info@kingkongmovers.nl>
2025-12-12 11:59:21 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/inv

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@
 // To switch to a different Firebase project, use the 'UpdateFirebaseProjectTool' in the Studio.
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: "gebr-stegeman-contracts.firebaseapp.com",
   projectId: "gebr-stegeman-contracts",
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
