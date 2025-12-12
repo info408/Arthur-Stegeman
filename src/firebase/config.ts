0cc7ac2cfb6d048826d1d973eb8f58c8cdfcf7f8
King Kong Movers - Gebr.Stegeman <info@kingkongmovers.nl>
2025-12-12 10:31:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1042886972-ded73",
+  "appId": "1:617185510964:web:004b71ca1c99ecc0326c61",
+  "apiKey": "AIzaSyC04GldjFwaS5mSFea0hRrvk1udzB_ydhA",
+  "authDomain": "studio-1042886972-ded73.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "617185510964"
+};
